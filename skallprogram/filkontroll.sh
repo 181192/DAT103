d33dc81002973367c0c485f7c3a9b98e63ac558b
181192 <181192@stud.hvl.no>
2017-09-01 09:51:29 -0400

    Added filkontroll.sh

--- skallprogram/filkontroll.sh
+++ skallprogram/filkontroll.sh
@@ -1,16 +1,31 @@
-#!/bin/bash
+TIME=$1
+FILE=$2
+RUNNING=1
+MD5="0"
 
-declare fil=$1
+if [ -f $FILE ]; then
+	MD5=$(md5sum $FILE)
+fi
 
-while true;
+while [ $RUNNING -eq 1 ];
 do
-   lesTid= stat -c%x $fil
-   echo $lesTid
-   sleep 2
-   echo "Sov i 2 sek"
-
+	sleep $TIME
+	#Checking if file has been created
+	if [ "$MD5" = "0" ]; then
+		if [ -f $FILE ]; then
+			echo "Filen $FILE ble opprettet."
+		fi
+	#File exists, doing checks of editing and deletion here
+	#Checking if file has been deleted
+	if [ ! -f $FILE ]; then
+		echo "Filen $FILE ble slettet."
+		continue;
+	fi
+	#Checking if file has been edited
+	if [[ $(md5sum $FILE) != $MD5 ]]; then
+		echo "Filen $FILE ble endret."
+		RUNNING=0;
+	fi
+fi
 done
-
-
-echo $fil
 exit 0
