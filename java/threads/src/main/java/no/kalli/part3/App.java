10f365a47cc0647ec9dd3f028c375f62e46e1b8a
181192 <181192@stud.hvl.no>
2017-12-09 18:31:28 +0100

    Tråd eksempel med synchonized på metoder

---
+++ java/threads/src/main/java/no/kalli/part3/App.java
@@ -0,0 +1,50 @@
+package no.kalli.part3;
+
+public class App {
+
+    private int count = 0;
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.doWork();
+    }
+
+    private void doWork() {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                loop();
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                loop();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Count is: " + count);
+    }
+
+    private void loop() {
+        for (int i = 0; i < 10000; i++) {
+            increment();
+        }
+    }
+
+    /**
+     * Synkroniserer inkrementasjonen av variabelen count
+     */
+    private synchronized void increment() {
+        count++;
+    }
+}
---
+++ java/threads/src/main/java/no/kalli/part3/README.md
@@ -0,0 +1,2 @@
+# Synchronize
+Tråd eksempel med bruk av synchronize på metoder
